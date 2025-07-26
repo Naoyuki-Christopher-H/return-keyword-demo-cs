40162123b1a4e4c346e790061bfd998aca17ac6d
YukiChriss <naoyuki.christopher.higaki@outlook.com>
2025-07-26 15:57:36 +0200

    Add project files.

---
+++ return-keyword-demo-cs/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace return_keyword_demo_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TOPIC:
+            // return keyword in C#
+
+            // DESCRIPTION:
+            // It returns data back to the place ehere a method was called.
+
+            try
+            {
+                Double ValueX;
+                Double ValueY;
+                Double Result;
+
+                Console.Write("Enter Value X: ");
+                ValueX = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter Value Y: ");
+                ValueY = Convert.ToDouble(Console.ReadLine());
+
+                Multiply(ValueX, ValueY);
+
+                Result = Multiply(ValueX, ValueY);
+                Console.WriteLine($"{ValueX} * {ValueY} = {Result}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERROR: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+
+        static Double Multiply(Double ValueX, Double ValueY)
+        {
+            try
+            {
+                Double ValueZ =  ValueX * ValueY;
+                return ValueZ;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"ERROR: {ex.Message}");
+                return 0;
+            }
+        }
+    }
+}
